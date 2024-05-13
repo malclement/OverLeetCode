703a8d633532293bf1eb4ff06535e37f87b083d8
ClementMalige <clement@kompozite.io>
2024-05-13 17:45:09 +0200

    python solution

---
+++ ScoreAfterFlippingMatrix/score_aftre_flipping_matrix.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def matrix_score(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+
+        for i in range(m):
+            if grid[i][0] == 0:
+                for j in range(n):
+                    grid[i][j] = 1 - grid[i][j]
+
+        for j in range(1, n):
+            count_ones = sum(grid[i][j] for i in range(m))
+            if count_ones < m / 2:
+                for i in range(m):
+                    grid[i][j] = 1 - grid[i][j]
+
+        # Calculate score
+        score = 0
+        for row in grid:
+            score += int("".join(map(str, row)), 2)
+
+        return score
